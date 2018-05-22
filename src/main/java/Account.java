ee3d81ddf70ccf252122e385152279ffd3bb6c4f
victorenaud <7222886+victorenaud@users.noreply.github.com>
2018-05-22 14:04:56 +0200

    Print statement with correct headers

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,5 @@
 public class Account {
+    public String generateStatement() {
+        return "date || credit || debit || balance";
+    }
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -4,7 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class AccountTest {
     @Test
-    void firstTest() {
-        assertThat(true).isFalse();
+    void shouldHaveAVoidStatement_onInit() {
+        // When
+        Account account = new Account();
+
+        // Then
+        String actual = account.generateStatement();
+        assertThat(actual).isEqualTo("date || credit || debit || balance");
     }
 }
